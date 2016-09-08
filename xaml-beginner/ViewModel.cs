ff582439b405ce0a01aa342cd3284ad18f221614
Bouzid Ghassen <bouzid.ghassen@gmail.com>
2016-09-08 21:50:30 +0100

    Add project files.

---
+++ xaml-beginner/ViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace xaml_beginner
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string propName=null)
+        {
+            if (PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propName));
+            }
+        }
+
+        private bool _showLoadingIndicator = false;
+
+        public bool ShowLoadingIndicator
+        {
+            get { return this._showLoadingIndicator; }
+            set
+            {
+                if (value != this._showLoadingIndicator)
+                {
+                    this._showLoadingIndicator = value;
+                    this.OnPropertyChanged();
+                }
+            }
+        }
+
+        public ViewModel()
+        {
+            this.ShowLoadingIndicator = true;
+        }
+    }
+}
